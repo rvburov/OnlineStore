bc891f0554063a83f6ec0e1122a268534135e99b
romanburov <romanburov@MacBook-Pro-romanburov.local>
2024-12-08 23:15:40 +0300

    Product category added

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -2,30 +2,32 @@ from django.shortcuts import render
 from django.views import View
 from .models import Product
 from cart.models import Cart
+from collections import defaultdict
 
 class ProductListView(View):
-    """Представление для отображения списка продуктов"""
+    """Представление для отображения списка продуктов по категориям"""
 
     template_name = "catalog/product_list.html"
     
     def get(self, request):
-        """Обрабатывает GET-запрос для отображения списка всех продуктов"""
+        """Обрабатывает GET-запрос для отображения продуктов по категориям"""
 
-        # Получаем все продукты из базы данных
+        # Получаем продукты и группируем их по категориям
         products = Product.objects.all()
+        products_by_category = defaultdict(list)
+        for product in products:
+            products_by_category[product.category].append(product)
         
         if request.user.is_authenticated:
-            # Если пользователь аутентифицирован, получаем продукты в корзине из базы данных
             cart_products = Cart.objects.filter(user=request.user)
             products_in_cart = cart_products.values_list('products_id', flat=True)
         else:
-            # Если пользователь не аутентифицирован, получаем корзину из сессии
             cart = request.session.get('cart', {})
             products_in_cart = cart.keys()
             cart_products = [{'products_id': int(product_id), 'weight': weight} for product_id, weight in cart.items()]
 
         context = {
-            "products": products,
+            "products_by_category": products_by_category.items(),  # Здесь важно вызвать `.items()`
             "products_in_cart": products_in_cart,
             "cart_products": cart_products,
         }
